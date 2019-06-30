f377baeb8ed34d01062404586d4df052abc6a28f
David Aaron Suddjian <aasuddjian@gmail.com>
2019-06-30 10:11:17 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Schedule cloud function calls with a simple API</h1>
+<h2>With eventually.run, you can schedule as many jobs as you want whenever you want.</h2>
+
+<h3>Perfect for stateless apps and cloud functions</h3>
+<p>Want to send a reminder message to your clients? Schedule it with eventually.run! Our trained elephants will remember to call your code, even if the event is years down the line.</p>
